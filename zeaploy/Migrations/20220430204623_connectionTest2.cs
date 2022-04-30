a47fba297277ed5c79820340999628b51e802848
reset-egs <souleater12456@gmail.com>
2022-04-30 22:48:19 +0200

    Setup Cloud Database Remove - Connection String from Appsettings.json

---
+++ zeaploy/Migrations/20220430204623_connectionTest2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace zeaploy.Migrations
+{
+    public partial class connectionTest2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
