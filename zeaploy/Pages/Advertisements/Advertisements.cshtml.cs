e8bd824b5a3d602583d3362beadb8e391eec9faa
reset-egs <souleater12456@gmail.com>
2022-05-10 10:47:31 +0200

    Add comment functionality Add - comment section on Details page Implement - ICommentService interface

--- zeaploy/Pages/Advertisements/Advertisements.cshtml.cs
+++ zeaploy/Pages/Advertisements/Advertisements.cshtml.cs
@@ -3,14 +3,12 @@ namespace zeaploy.Pages.Advertisements
     public class AdvertisementsModel : PageModel
     {
         private IAdvertisementService adService;
-        private readonly INotyfService notyf;
 
         public IEnumerable<Advertisement> Advertisements { get; set; }
 
-        public AdvertisementsModel(IAdvertisementService service, INotyfService notyf)
+        public AdvertisementsModel(IAdvertisementService service)
         {
             adService = service;
-            this.notyf = notyf;
         }
         public async Task OnGetAsync()
         {
