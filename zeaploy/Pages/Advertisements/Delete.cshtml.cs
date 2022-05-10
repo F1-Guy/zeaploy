3496d0e1e612a5326a2704f829bddd5f9c302a40
Tea Koscina <teax0669@edu.zealand.dk>
2022-05-10 11:37:24 +0200

    add - edit and delete page - code finished

---
+++ zeaploy/Pages/Advertisements/Delete.cshtml.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace zeaploy.Pages.Advertisements
+{
+    [Authorize(Roles = "Admin")]
+    public class DeleteModel : PageModel
+    {
+        private IAdvertisementService adService;
+        public DeleteModel(IAdvertisementService service)
+        {
+            adService = service;
+        }
+        [BindProperty]
+        public Advertisement Advertisement {get;set;}
+
+        public async Task OnGetAsync(int advertisementId)
+        {
+            Advertisement = await adService.GetAdvertisementByIdAsync(advertisementId);
+        }
+        public async Task<IActionResult> OnPostAsync(int advertisementId)
+        {
+            await adService.DeleteAdvertisementAsync(advertisementId);
+            return RedirectToPage("/Advertisements/Advertisements");
+        }
+    }
+}
