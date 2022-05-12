a226eb2dac66d2a0faab266c7bc40113e660ad72
reset-egs <souleater12456@gmail.com>
2022-05-12 12:43:50 +0200

    Configure uploading Company logo for advertisements Add - UploadCompanyLogo method Edit - Create and Advertisements pages

--- zeaploy/Pages/Advertisements/Create.cshtml.cs
+++ zeaploy/Pages/Advertisements/Create.cshtml.cs
@@ -5,15 +5,20 @@ namespace zeaploy.Pages.Advertisements
     {
         private readonly IAdvertisementService service;
         private readonly INotyfService notyfService;
+        private readonly IFileService fileService;
 
-        public CreateModel(IAdvertisementService service, INotyfService notyfService)
+        public CreateModel(IAdvertisementService service, INotyfService notyfService, IFileService fileService)
         {
             this.service = service;
             this.notyfService = notyfService;
+            this.fileService = fileService;
         }
 
         [BindProperty]
         public Advertisement Advertisement { get; set; }
+
+        [BindProperty]
+        public IFormFile? Logo { get; set; }
 
         public void OnGet()
         {
@@ -22,6 +27,8 @@ namespace zeaploy.Pages.Advertisements
         public async Task<IActionResult> OnPostAsync()
         {
             Advertisement.Posted = DateTime.Now;
+            await fileService.UploadCompanyLogoAsync(Logo, Advertisement.Company);
+            Advertisement.ImagePath = Logo.FileName;
             await service.CreateAdvertisementAsync(Advertisement);
             notyfService.Success("You have successfully added an advertisement.");
             return RedirectToPage("Advertisements");
--- zeaploy/Services/Services/FileService.cs
+++ zeaploy/Services/Services/FileService.cs
@@ -13,6 +13,17 @@
             }
         }
 
+        public async Task UploadCompanyLogoAsync(IFormFile companyLogo, string name)
+        {
+            string relativePath = $@"wwwroot\company-logos\{name}\";
+            Directory.CreateDirectory(relativePath);
+            var file = Path.Combine(relativePath, companyLogo.FileName);
+            using (var fileStream = new FileStream(file, FileMode.Create))
+            {
+                await companyLogo.CopyToAsync(fileStream);
+            }
+        }
+
         public async Task UploadProfilePictureAsync(IFormFile ProfilePicture, string name)
         {
             string relativePath = $@"wwwroot\user-data\profile-pictures\{name}\";
