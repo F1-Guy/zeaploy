346a7d560ec12a408cd510990419461533f386d4
F1-Guy <bartol.cagalj@gmail.com>
2022-05-23 14:25:10 +0200

    Update delete pages Change - update UI of delete pages

--- zeaploy/Pages/Advertisements/Delete.cshtml.cs
+++ zeaploy/Pages/Advertisements/Delete.cshtml.cs
@@ -14,9 +14,17 @@ namespace zeaploy.Pages.Advertisements
         [BindProperty]
         public Advertisement Advertisement { get; set; }
 
+        [Display(Name = "Short Description")]
+        public IEnumerable<string> ShortDesc { get; set; }
+
         public async Task OnGetAsync(int advertisementId)
         {
             Advertisement = await adService.GetAdvertisementByIdAsync(advertisementId);
+            ShortDesc = Advertisement.Description.Split(new char[] { '.', '?', '!', '\n'});
+            if (ShortDesc.Count() >= 3)
+            {
+                ShortDesc = ShortDesc.Take(3);
+            }
         }
         public async Task<IActionResult> OnPostAsync(int advertisementId)
         {
