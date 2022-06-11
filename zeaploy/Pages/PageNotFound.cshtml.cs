76127bc898c2bfcf288b4daeaeef8d68482c70e8
Bartol Cagalj <bartol.cagalj@gmail.com>
2022-06-11 21:11:55 +0200

    Nice to have features Add - new index page with placeholders Add - friendly URL route names Add - page PageNotFound Add - incorrect paths redirect to page not found Change - search button placement in advertisement page Change - Other minor changes

---
+++ zeaploy/Pages/PageNotFound.cshtml.cs
@@ -0,0 +1,9 @@
+namespace zeaploy.Pages.Account
+{
+    public class PageNotFoundModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- zeaploy/Program.cs
+++ zeaploy/Program.cs
@@ -2,7 +2,27 @@ var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages()
+                .AddRazorPagesOptions(options =>
+                {
+                    // Redirects any URL that doesn't map to anything and redirects to index
+                    options.Conventions.AddPageRoute("/PageNotFound", "{*url}");
+                    options.Conventions.AddPageRoute("/PageNotFound", "ERROR:404 Page Not Found");
+
+                    options.Conventions.AddPageRoute("/Advertisements/Advertisements", "Advertisements");
+                    options.Conventions.AddPageRoute("/Applications/Applications", "Applications");
+                    options.Conventions.AddPageRoute("/Profile/Profile", "Profile");
+                    options.Conventions.AddPageRoute("/Users/Users", "Users");
+
+                    options.Conventions.AddPageRoute("/Account/Login", "Login");
+                    options.Conventions.AddPageRoute("/Account/Register", "Register");
+                    options.Conventions.AddPageRoute("/Account/Logout", "Logout");
+                    options.Conventions.AddPageRoute("/Account/AccessDenied", "ERROR:403 Access Denied");
+
+                    options.Conventions.AddPageRoute("/Privacy", "Privacy Policy");
+                });
+
+
 builder.Services.AddDbContext<ZeaployDbContext>(options => options.UseSqlServer(config.GetConnectionString("ZeaployConnection")));
 builder.Services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<ZeaployDbContext>();
 builder.Services.AddScoped<IAdvertisementService, AdvertisementService>();
